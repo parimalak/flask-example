671c9bd6c23c9ace88de3ab0980ef152661ad969
parimalak <parimala.killada@rockets.utoledo.edu>
2017-04-10 23:00:35 -0400

    Procfile

--- routes.py
+++ routes.py
@@ -1,10 +1,16 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
+  return render_template("index.html")
 
-if __name__=="__main__":
-	app.run(debug = True)
+@app.route("/about")
+def about():
+  return render_template("about.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
 	
