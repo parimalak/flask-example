ae0ac0f16a33ae1b1781faeb0c4e7597f9a59e9b
parimalak <parimala.killada@rockets.utoledo.edu>
2017-04-12 16:57:44 -0400

    Added signup,login and authorisation

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request,session,redirect,url_for
+from models import db, User
+from forms import SignupForm,LoginForm
+
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Stack406@localhost:5432/learningflask'
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 @app.route("/")
 def index():
@@ -10,6 +17,56 @@ def index():
 def about():
   return render_template("about.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+  if 'email' in session:
+  	return redirect(url_for('home'))
+  
+  form = SignupForm()
+
+  if request.method == "POST":
+    if form.validate():
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      session['email'] =newuser.email
+      return redirect(url_for('home'))
+    else:
+      return render_template('signup.html', form=form)
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+  if 'email' in session:
+  	return redirect(url_for('home'))
+  form = LoginForm()
+
+  if request.method == "POST":
+    if form.validate():
+      email = form.email.data
+      password = form.password.data
+      user = User.query.filter_by(email=email).first()
+      if user is not None and user.check_password(password):
+         session['email'] =user.email
+         return redirect(url_for('home'))
+      else:
+          return redirect(url_for('login'))
+    else:
+      return render_template('login.html', form=form)
+  elif request.method == "GET":
+    return render_template('login.html', form=form)
+
+@app.route("/logout")
+def logout():
+	session.pop('email',None)
+	return redirect(url_for('index'))
+
+@app.route("/home")
+def home():
+	if 'email' not in session:
+		return redirect(url_for('login'))
+	return render_template("home.html")
 if __name__ == "__main__":
   app.run(debug=True)
 
